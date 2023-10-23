fbd3c6fe6bcf5f5a4091bf40b3187c1a186fb254
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-23 12:31:05 +0530

    add rate limit

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
 const PORT = process.env.PORT || 3006;
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 5,
+});
+
 app.use(morgan("combined"));
+app.use(limiter);
+
 app.use(
   "/bookingservice",
   createProxyMiddleware({
