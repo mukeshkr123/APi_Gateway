3426c5d5d7bf1d7f1c0ee874486bb1f87d522ea2
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-23 12:59:50 +0530

    add authservice for bookingservice

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const rateLimit = require("express-rate-limit");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -14,6 +15,33 @@ const limiter = rateLimit({
 
 app.use(morgan("combined"));
 app.use(limiter);
+
+app.use("/bookingservice", async (req, res, next) => {
+  console.log(req.headers["x-access-token"]);
+  try {
+    const response = await axios.get(
+      "http://localhost:3001/api/v1/isauthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
+
+    console.log(response.data);
+    if (response.data.success) {
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Unauthorised",
+      });
+    }
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorised",
+    });
+  }
+});
 
 app.use(
   "/bookingservice",
