e8f58d698848b1078b5dfc1d58d1cb610425d6c5
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-23 12:27:43 +0530

    add proxy for booking-service

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3006;
+
+app.use(morgan("combined"));
+app.use(
+  "/bookingservice",
+  createProxyMiddleware({
+    target: "http://localhost:3002/",
+    changeOrigin: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  return res.json({
+    msg: "ok",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
